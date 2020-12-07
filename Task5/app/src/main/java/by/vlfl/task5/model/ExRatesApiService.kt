26e3911aa9a180fed9000dfb6e0f1c27b0a6474d
Uladzislau Flaryanovich <vlfl2612@yandex.by>
2020-12-07 13:27:38 +0300

    add: task 5 implementation

---
+++ Task5/app/src/main/java/by/vlfl/task5/model/ExRatesApiService.kt
@@ -0,0 +1,33 @@
+package by.vlfl.task5.model
+
+import by.vlfl.task5.model.db.RateEntity
+import retrofit2.http.GET
+
+interface ExRatesApiService {
+    @GET("exrates/currencies")
+    suspend fun getRates(): List<Rate>
+}
+
+fun List<Rate>.asRateEntity(): List<RateEntity> {
+    return map{
+        RateEntity(
+            it.curID,
+            it.curParentID,
+            it.curCode,
+            it.curAbbreviation,
+            it.curName,
+            it.curNameBel,
+            it.curNameEng,
+            it.curQuotName,
+            it.curQuotNameBel,
+            it.curQuotNameEng,
+            it.curNameMulti,
+            it.curNameBelMulti,
+            it.curNameEngMulti,
+            it.curScale,
+            it.curPeriodicity,
+            it.curDateStart,
+            it.curDateEnd,
+        )
+    }
+}
